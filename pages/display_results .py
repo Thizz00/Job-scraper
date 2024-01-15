6e00ed077154499e06bde2f0c945783df0058cd0
Thizz00 <jakubkielb7@gmail.com>
2024-01-15 23:07:09 +0100

    clear code

--- pages/display_results .py
+++ pages/display_results .py
@@ -1,13 +1,16 @@
-import streamlit as st
 import os
 import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import streamlit as st
 from resume_checker.data_fetcher import get_technologies_from_database
 from Logs.logging_setup import initialize_logging
+
 st.sidebar.markdown("# Display results ❄️")
 
 initialize_logging()
+
 def prepare_df(df):
     category_words = set(word.strip().upper() for word in df['category'] if word is not None)
     tech_stack_words = set(word.strip().upper() for word in df['tech_stack'] if word is not None)
@@ -27,12 +30,16 @@ def contains_all_search_words(row, selected_search_words):
 
 df = get_technologies_from_database()
 search_words = prepare_df(df)
+
 if df is not None and not df.empty:
     st.markdown("<h2 style='text-align: center;'>Enter words to search for:</h2>", unsafe_allow_html=True)
+    
     selected_search_words = st.multiselect('', search_words)
+    
     st.markdown("<h2 style='text-align: center;'>All job offers:</h2>", unsafe_allow_html=True)
 
     filtered_df = df[df.apply(contains_all_search_words, axis=1, selected_search_words=selected_search_words)]
+    
     st.data_editor(
         filtered_df,
         column_config={
--- pages/display_results_match.py
+++ pages/display_results_match.py
@@ -1,18 +1,20 @@
-import streamlit as st
 import os
 import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import streamlit as st
 from Logs.logging_setup import initialize_logging
 from database.database_initializer import initialize_database
 import pandas as pd
 
 st.sidebar.markdown("# Display match results ❄️")
+
 initialize_logging()
 
 def get_technologies_from_database():
     engine, session = initialize_database()
-    query = "SELECT * FROM tech_tools order by matched DESC"
+    query = "SELECT * FROM tech_tools ORDER BY matched DESC"
     df = pd.read_sql(query, engine)
     return df
 
@@ -33,5 +35,5 @@ else:
     col1, col2, col3 = st.columns(3)
     col1.write("")
     with col2:
-        st.markdown("<h2 style='text-align: center;'>No job offers. </h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>No job offers.</h2>", unsafe_allow_html=True)
     col3.write("")
