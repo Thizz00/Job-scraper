ef43fe0792c06e99716467688058868be1cd848b
Jakub Kiełb <97180137+Thizz00@users.noreply.github.com>
2024-02-02 12:19:57 +0100

    Wait argument added to run_scraper.ps1

--- run_scraper.ps1
+++ run_scraper.ps1
@@ -1,5 +1,5 @@
 Set-Location -Path ("full path do job scraper")
-$scraper = Start-Process python -ArgumentList "scripts\main_nostreamlit.py --job main_job_scraper --match resume_data_processor" -NoNewWindow -PassThru
+$scraper = Start-Process python -ArgumentList "scripts\main_nostreamlit.py --job main_job_scraper --match resume_data_processor" -NoNewWindow -PassThru -Wait
 $scraper
 
 Stop-Process -Id $scraper.Id
