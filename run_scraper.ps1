088163af27d98bacd0049e3989b9f661e06e07f4
Jakub Kiełb <jakubkielb7@gmail.com>
2024-03-07 16:44:15 +0100

    Check if folder Logs exists

--- run_scraper.ps1
+++ run_scraper.ps1
@@ -1,4 +1,4 @@
-Set-Location -Path ("full path do job scraper")
+Set-Location -Path ("Full path")
 $scraper = Start-Process python -ArgumentList "scripts\main_nostreamlit.py --job main_job_scraper --match resume_data_processor" -NoNewWindow -PassThru -Wait
 $scraper
 
